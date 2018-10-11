5cc648e470bd7b0a187525c0da883ae349ddcf7b
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2018-10-11 15:35:20 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("first test OK");
+        } else {
+            System.out.println("Error! - first test");
+        }
+
+        Calculator calculator = new Calculator();
+        int a = 37;
+        int b = 15;
+
+
+        if (calculator.add(a, b) == (a + b) && calculator.substract(a, b) == (a - b)) {
+            System.out.println("second test OK");
+        } else {
+            System.out.println("Error! - second test");
+        }
     }
 }
