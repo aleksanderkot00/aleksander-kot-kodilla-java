bb98f9dc62a755cd03ea7707be0186aa101a5b19
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2018-10-31 16:50:18 +0100

    8.3 - poprawa wyswietlanego komiunikatu

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -1,4 +1,25 @@
 package com.kodilla.exception.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlightFinder {
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+
+        Map<String, Boolean> arrivalAirports = new HashMap<>();
+        arrivalAirports.put("London", true);
+        arrivalAirports.put("Berlin", true);
+        arrivalAirports.put("Warsaw", false);
+        arrivalAirports.put("Vienna", true);
+        arrivalAirports.put("New York", false);
+        arrivalAirports.put("Prague", false);
+        arrivalAirports.put("Munich", true);
+        arrivalAirports.put("Poznan", false);
+
+        if (arrivalAirports.containsKey(flight.getArrivalAirport())) {
+            System.out.println("Flight to " + flight.getArrivalAirport() + ": " + arrivalAirports.get(flight.getArrivalAirport()));
+        } else {
+            throw new RouteNotFoundException("Introduced key isn't on the map!");
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -1,4 +1,22 @@
 package com.kodilla.exception.test;
 
 public class FlightFinderRunner {
+    public static void main(String[] args) {
+
+        Flight flight1 = new Flight("London", "Berlin");
+        Flight flight2 = new Flight("Warsaw", "Madrid");
+
+        FlightFinder flightFinder = new FlightFinder();
+        try {
+            flightFinder.findFilght(flight1);
+        } catch (RouteNotFoundException e) {
+            System.out.println("We don't have information about this arrival airport!");
+        }
+
+        try {
+            flightFinder.findFilght(flight2);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e + " We don't have information about this arrival airport!");
+        }
+    }
 }
