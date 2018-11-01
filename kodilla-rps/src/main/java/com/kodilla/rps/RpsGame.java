e4d6517f89f9e614a7afa62eb34b37830d1240e9
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2018-11-02 00:51:36 +0100

    RPS - poprawa wcięć

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
@@ -1,17 +1,22 @@
 package com.kodilla.rps;
+
 import java.util.Random;
 import java.util.Scanner;
+
 public class RpsGame {
+
     private String playerName;
     private int roundsNumber;
     private int playerWinsNumber = 0;
     private int computerWinsNumber = 0;
     private boolean end = false;
     private Scanner scanner = new Scanner(System.in);
+
     public RpsGame(String playerName, int roundsNumber) {
         this.playerName = playerName;
         this.roundsNumber = roundsNumber;
     }
+
     private void round(int computerMove, int playerMove) {
         if (playerMove == 1 && computerMove == 2) {
             computerWinsNumber++;
@@ -41,6 +46,7 @@ public class RpsGame {
             System.out.println("Draw!");
         }
     }
+
     public void game() {
         while (!end) {
             stateOfGame();
@@ -48,12 +54,14 @@ public class RpsGame {
             userChoiceChecker(userChoice);
         }
     }
+
     private void newGame() {
         System.out.println("How many rounds would you like to play to?");
         roundsNumber = scanner.nextInt();
         playerWinsNumber = 0;
         computerWinsNumber = 0;
     }
+
     private void endOfGame() {
         System.out.println("Are you sure that you wont end this game? y - yes; n - no");
         if (scanner.nextLine().equals("y")) {
@@ -61,23 +69,36 @@ public class RpsGame {
             System.out.println("End of the game.");
         }
     }
+
     private int computerMoveGenerator() {
         Random generator = new Random();
         return generator.nextInt(3) + 1;
     }
+
     private void stateOfGame() {
         System.out.println("\nScore: computer - " + computerWinsNumber + "; " + playerName + " - " + playerWinsNumber);
-        System.out.println("1 - rock; 2 - paper; 3 - scissors; x - end of the game; n - new game");
+        System.out.println("1 - r" +
+                "ock; 2 - paper; 3 - scissors; x - end of the game; n - new game");
     }
+
     private void userChoiceChecker(String userChoice) {
         if  ( userChoice.equals("1") || userChoice.equals("2") || userChoice.equals("3") ) {
             int userMove = Integer.parseInt(userChoice);
             round(computerMoveGenerator(), userMove);
             if (playerWinsNumber == roundsNumber || computerWinsNumber == roundsNumber) {
-                end = true;
                 System.out.println("Score: computer - " + computerWinsNumber + "; " + playerName + " - " + playerWinsNumber);
-                if (playerWinsNumber > computerWinsNumber) System.out.println("You won the game!");
-                else System.out.println("You lost the game!");
+                if (playerWinsNumber > computerWinsNumber) {
+                    System.out.println("You won the game!");
+                }
+                else {
+                    System.out.println("You lost the game!");
+                }
+                System.out.println("Would you like to play again?");
+                if (scanner.nextLine().equals("y")) {
+                    newGame();
+                } else {
+                    endOfGame();
+                }
             }
         } else if (userChoice.equals("x")) {
             endOfGame();
