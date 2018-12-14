29db51b0502faf00af31c3e7ccf3b4156b18d2e7
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2018-12-14 11:20:53 +0100

    Moduł 14.3 oraz dodanie testu dzielenia przez 0 w zadaniu 14.2

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -18,10 +18,12 @@ public class CalculatorTestSuite {
         double sub = calculator.sub(2,3);
         double mul = calculator.mul(4,12);
         double div = calculator.div(2,4);
+        double div2 = calculator.div(2,0);
         //Then
         Assert.assertEquals(13.0, add, 0.0001);
         Assert.assertEquals(-1.0, sub, 0.0001);
         Assert.assertEquals(48.0, mul, 0.0001);
         Assert.assertEquals(0.5, div, 0.0001);
+        Assert.assertTrue(Double.isInfinite(div2));
     }
 }
