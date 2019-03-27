327aaeec1b80c10f11076fbe7a358be976ec366d
unknown <aleksanderkot00@gmail.com>
2019-03-28 00:37:50 +0100

    Zadanie 28.3

--- kodila-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodila-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 
 @Service
-//@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy
 public class OrderFacade {
 
     @Autowired
