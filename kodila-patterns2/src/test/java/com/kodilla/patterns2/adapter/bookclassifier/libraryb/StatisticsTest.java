5394e38ccbfa4598ccdf276bdfcb6258ee582fc4
unknown <aleksanderkot00@gmail.com>
2019-03-28 20:50:59 +0100

    Przepisanie klas z zadania

---
+++ kodila-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/libraryb/StatisticsTest.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns2.adapter.bookclassifier.libraryb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+    @Test
+    public void testSss() {
+        int[] ints = {1, 3, 23, 43, 22, 5, 123};
+        System.out.println((int) 2.5);
+    }
+
+}
