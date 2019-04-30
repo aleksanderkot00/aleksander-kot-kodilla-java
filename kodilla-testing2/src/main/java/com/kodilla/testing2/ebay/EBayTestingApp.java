de629ea5806e0ce655a30bea9541ff8330addecc
unknown <aleksanderkot00@gmail.com>
2019-04-30 02:53:59 +0200

    Zadanie 31.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,19 +1,19 @@
-package com.kodilla.testing2.google;
+package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleTestingApp {
-    public static final String SEARCHFIRLDNAME = "q";
+public class EBayTestingApp {
+    public static final String SEARCHFIRLDNAME = "_nkw";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
-        driver.get("https://www.google.com/");
+        driver.get("https://www.ebay.com/");
 
         WebElement searchField = driver.findElement(By.name(SEARCHFIRLDNAME));
-        searchField.sendKeys("Kodilla");
+        searchField.sendKeys("Laptop");
         searchField.submit();
     }
 }
